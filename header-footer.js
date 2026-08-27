f33ef921a24ec6c1d4bd5a705b9821cc64f669dc
ONYX <yamenauzeed@gmail.com>
2026-08-27 20:20:01 +0300

    Update header-footer.js

--- header-footer.js
+++ header-footer.js
@@ -21,8 +21,7 @@ let footer = `
         not associated in any way to OMOCAT, LLC. If the owner of the intellectual property of the brand
         OMORI wants to contact us, here is the team's e-mail address: <a
             href="mailto:omori.arabic@gmail.com">omori.arabic@gmail.com</a></p>
-    <p class="disclaimer-text">Website made by kitsou with help from MAZ.
-    Edited and improved by Black.</p>
+    <p class="disclaimer-text">Website made by kitsou with help from MAZ.</b>Edited and improved by Black.</p>
 </div>
 `
 
